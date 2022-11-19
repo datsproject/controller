c40f0dc84e6111b172d3e39202580aa19c1856ce
burakvural <burakvural93@gmail.com>
2022-11-19 22:29:01 +0300

    deploy to dockerhub has been added

--- deploy_to_dockerhub.sh
+++ deploy_to_dockerhub.sh
@@ -1,6 +1,6 @@
 mvn clean -Dspring.profiles.active=test
 mvn install -Dspring.profiles.active=test -Dmaven.test.skip=true
 
-docker build -f test.Dockerfile -t dats_ddos_middleware .
-docker image tag dats_ddos_middleware burakvural93/dats_ddos_middleware:latest
-docker image push burakvural93/dats_ddos_middleware:latest
+docker build -f test.Dockerfile -t dats_ddos_controller .
+docker image tag dats_ddos_controller burakvural93/dats_ddos_controller:latest
+docker image push burakvural93/dats_ddos_controller:latest
